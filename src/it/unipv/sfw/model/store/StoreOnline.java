969e74219e5ecf0c720e08e55a42c66ee9c5209e
FedeRoma01 <federico.romano01@universitadipavia.it>
2023-01-28 10:35:42 +0100

    Aggiunta documentazione per Abbonamento, TipoAbb, Merchandising e StoreOnilne.

--- src/it/unipv/sfw/model/store/StoreOnline.java
+++ src/it/unipv/sfw/model/store/StoreOnline.java
@@ -3,13 +3,24 @@ package it.unipv.sfw.model.store;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Classe che rappresenta lo Store del sito per l'acquisto del {@link Mechandising}.
+ * @author Federico Romano
+ * @see Merchandising
+ */
 public class StoreOnline {
 	private List<Merchandising> archivioMerch;
 
 	public StoreOnline() {
 		archivioMerch = new ArrayList<Merchandising>();
 	}
-
+	
+	/**
+	 * Funzione che permette di aggiungere un merch di una certa quantità passandoli come parametri.
+	 * @param merchItem
+	 * @param quantita
+	 */
 	public void addMerch(Merchandising merchItem, int quantita) {
 		int i;
 		for (i = 0; i < quantita; i++) {
@@ -17,6 +28,11 @@ public class StoreOnline {
 		}
 	}
 
+	/**
+	 * Funzione che permette di rimuovere totalmente dallo store 
+	 * il merch passato come parametro
+	 * @param merchItem
+	 */
 	public void removeMerch(Merchandising merchItem) {
 		for (Merchandising merch : archivioMerch) {
 			if (merch.compareTo(merchItem) == 0) {
@@ -25,6 +41,11 @@ public class StoreOnline {
 		}
 	}
 
+	/**
+	 * @param merchItem
+	 * @return Il numero di merch uguali a quello passato
+	 * come parametro ancora disponibili.
+	 */
 	public int getDisponibilita(Merchandising merchItem) {
 		int itemCount = 0;
 		for (Merchandising merch : archivioMerch) {
