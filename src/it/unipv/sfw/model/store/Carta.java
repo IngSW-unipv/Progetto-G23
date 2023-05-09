a5a703b9312ffda80a3c3f569315118d94c46e71
adLore18 <lorenzo.reale01@universitadipavia.it>
2023-05-09 15:05:50 +0200

    Creazione della classe Carta per memorizzare i metodi di pagamento.

---
+++ src/it/unipv/sfw/model/store/Carta.java
@@ -0,0 +1,64 @@
+package it.unipv.sfw.model.store;
+
+public class Carta {
+	
+	private String nome, cognome, paese, dataScadenza;
+	private int nCartaCredito, CVV;
+	
+	public Carta(String nome, String cognome, String paese, int nCartaCredito, String dataScadenza, int CVV) {
+		this.cognome = cognome;
+		this.CVV = CVV;
+		this.nome = nome;
+		this.paese = paese;
+		this.dataScadenza = dataScadenza;
+		this.nCartaCredito = nCartaCredito;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	
+	public int getCVV() {
+		return CVV;
+	}
+	
+	public String getDataScadenza() {
+		return dataScadenza;
+	}
+	
+	public int getnCartaCredito() {
+		return nCartaCredito;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getPaese() {
+		return paese;
+	}
+	
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public void setCVV(int cVV) {
+		CVV = cVV;
+	}
+	
+	public void setDataScadenza(String dataScadenza) {
+		this.dataScadenza = dataScadenza;
+	}
+	
+	public void setnCartaCredito(int nCartaCredito) {
+		this.nCartaCredito = nCartaCredito;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setPaese(String paese) {
+		this.paese = paese;
+	}
+}
