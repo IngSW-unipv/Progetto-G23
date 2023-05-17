624be5be7a8db167a5b4608070591c87899a4dec
adLore18 <lorenzo.reale01@universitadipavia.it>
2023-05-17 11:26:23 +0200

    Creazione del metodo per l'invio delle mail nella classe Pagamento.

--- src/it/unipv/sfw/model/store/Pagamento.java
+++ src/it/unipv/sfw/model/store/Pagamento.java
@@ -1,15 +1,24 @@
 package it.unipv.sfw.model.store;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class Pagamento {
-	
-	private int prezzo;
+	final String MITTENTE = "StadiumSystem@gmail.com";
+
+	private ArrayList <Merchandising> ordini;
 	private Carta metodoPagamento;
 	
-	public Pagamento(int prezzo, Carta metodoPagamento) {
-		this.prezzo = prezzo;
+	public Pagamento(Carta metodoPagamento, Merchandising merch) {
+		ordini = new ArrayList<Merchandising>();
+		ordini.add(merch);
 		this.metodoPagamento = metodoPagamento;
 	}
 	
@@ -17,16 +26,37 @@ public class Pagamento {
 		return metodoPagamento;
 	}
 	
-	public int getPrezzo() {
-		return prezzo;
-	}
-	
 	public void setMetodoPagamento(Carta metodoPagamento) {
 		this.metodoPagamento = metodoPagamento;
 	}
 	
-	public void setPrezzo(int prezzo) {
-		this.prezzo = prezzo;
+	public void addOrdine(Merchandising merch) {
+		ordini.add(merch);
+	}
+	
+	public void sendMail(String destinatario) throws MessagingException{
+		
+		String messaggio = "";
+		messaggio += "ID          NOME        PREZZO      \n";
+		
+		for(int i=0; i<ordini.size(); i++) {
+			messaggio += ordini.get(i).toString();
+		}
+		
+		Properties props = new Properties();
+	    props.put("mail.smtp.host", "smtp.example.com");
+	 
+	    Session session = Session.getDefaultInstance(props, null);
+	    Message msg = new MimeMessage(session);
+	 
+	    InternetAddress addressFrom = new InternetAddress(MITTENTE);
+	    msg.setFrom(addressFrom);
+	    InternetAddress addressTo = new InternetAddress(destinatario); 
+	  
+	    msg.setSubject("Ordine StadiumSystem");
+	    msg.setContent(messaggio, "text/plain");
+	  
+	    Transport.send(msg);
 	}
 
 }
