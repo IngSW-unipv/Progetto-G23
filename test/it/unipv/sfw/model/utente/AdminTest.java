b7ddba76a8615b1697763271aff86a0b070508c6
Piccoli01 <jacopo.piccoli01@universitadipavia.it>
2023-06-11 13:50:31 +0200

    Modificato Admin Test dopo le modifiche della classe admin.

--- test/it/unipv/sfw/model/utente/AdminTest.java
+++ test/it/unipv/sfw/model/utente/AdminTest.java
@@ -6,6 +6,8 @@ package it.unipv.sfw.model.utente;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Calendar;
+
 import org.junit.jupiter.api.Test;
 
 import it.unipv.sfw.exceptions.EmptyFieldException;
@@ -23,7 +25,7 @@ class AdminTest {
 	 */
 	@Test
 	void testGetType() {
-		Admin a = new Admin(null, null, null, null);
+		Admin a = new Admin(null, null, null, null,null);
 		assertEquals(a.getType(), Utente.Type.ADMIN);
 	}
 
@@ -32,10 +34,10 @@ class AdminTest {
 	 */
 	@Test
 	void testCheckValidity() {
-		Admin empty = new Admin(" ", "", "", "\t");
+		Admin empty = new Admin(" ", "", "", "\t",null);
 		assertThrows(EmptyFieldException.class, () -> empty.checkValidity());
 		
-		Admin invalidFormat = new Admin("a", "b", "invalid", "c");
+		Admin invalidFormat = new Admin("a", "b", "invalid", "c",Calendar.getInstance());
 		assertThrows(WrongEmailFormatException.class, () -> invalidFormat.checkValidity());
 	}
 
