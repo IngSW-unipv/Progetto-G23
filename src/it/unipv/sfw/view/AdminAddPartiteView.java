7dda6b45b2f9913ea32a063649f956e056322f5c
Piccoli01 <jacopo.piccoli01@universitadipavia.it>
2023-06-16 17:15:42 +0200

    Rimossi errori con la data.

--- src/it/unipv/sfw/view/AdminAddPartiteView.java
+++ src/it/unipv/sfw/view/AdminAddPartiteView.java
@@ -11,6 +11,7 @@ import java.awt.Insets;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -18,105 +19,100 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.border.Border;
 
 import com.github.lgooddatepicker.components.DatePicker;
-import com.github.lgooddatepicker.components.DatePickerSettings;
-import com.github.lgooddatepicker.components.DateTimePicker;
-import com.github.lgooddatepicker.components.TimePickerSettings;
 
 import it.unipv.sfw.model.partita.Partita.Squadre;
 import it.unipv.sfw.view.buttons.SquadraButton;
-
+import it.unipv.sfw.view.buttons.UtenteButton;
+ 
 public class AdminAddPartiteView extends AView {
-
-	private String[] opzioni = { "12:30", "15:00", "18:00", "18:30", "20:45", "21:00" };
-
+	
+	private String[] opzioni= {"12:30","15:00","18:00","18:30","20:45","21:00"};
+	
 	private ArrayList<JLabel> nomesquadra;
 	private ArrayList<ImageIcon> imgStemma;
 	private ArrayList<SquadraButton> sceltasquadra;
-	private ArrayList<JPanel> squadra;
+	private ArrayList<JPanel>squadra;
 	private JComboBox<String> orario;
-	private JButton aggiungi, home;
+	private JButton aggiungi,home;
 	private JLabel errorLabel;
-	private DateTimePicker data;
-
+	private DatePicker data;
+	
 	public AdminAddPartiteView(Dimension dim) {
-
-		Font medium = new Font("Arial", 1, 18);
-		Font large = new Font("Arial", 1, 24);
-
-		nomesquadra = new ArrayList<>();
-		squadra = new ArrayList<>();
-		imgStemma = new ArrayList<>();
-		sceltasquadra = new ArrayList<>();
-		data = new DateTimePicker();
-		LocalDate today = LocalDate.now();
-		DatePickerSettings dateSettings = new DatePickerSettings();
-		TimePickerSettings timeSettings = new TimePickerSettings();
-		data = new DateTimePicker(dateSettings, timeSettings);
-		data.setPreferredSize(new Dimension(300, 30)); ////
-		dateSettings.setDateRangeLimits(today.plusDays(1), today.plusDays(70));
-		orario = new JComboBox<>(opzioni);
-
-		JLabel titolo = new JLabel("AGGIUNGI UNA PARTITA");
+		
+		Font medium=new Font("Arial", 1, 18);
+		Font large=new Font("Arial",1,24);
+		
+		nomesquadra=new ArrayList<JLabel>();
+		squadra=new ArrayList<JPanel>();
+		imgStemma=new ArrayList<ImageIcon>();
+		sceltasquadra=new ArrayList<SquadraButton>();
+		data=new DatePicker();
+		data.setDateToToday();
+		orario=new JComboBox<String>(opzioni);
+		
+		
+		
+		JLabel titolo=new JLabel("AGGIUNGI UNA PARTITA");
 		titolo.setFont(large);
-		titolo.setHorizontalAlignment(SwingConstants.CENTER);
+		titolo.setHorizontalAlignment(JLabel.CENTER);
 		titolo.setBackground(Color.BLUE);
 		titolo.setOpaque(true);
-
-		home = new JButton("");
-		ImageIcon imgHome = new ImageIcon((new ImageIcon(this.getClass().getResource("/home.png"))).getImage());
+		
+		home=new JButton("");
+		ImageIcon imgHome=new ImageIcon((new ImageIcon(this.getClass().getResource("/home.png"))).getImage());
 		home.setIcon(imgHome);
-
-		JPanel top = new JPanel();
+		
+		JPanel top=new JPanel();
 		top.setLayout(new BorderLayout());
-		top.add(titolo, BorderLayout.CENTER);
-		top.add(home, BorderLayout.EAST);
-
-		for (Squadre s : Squadre.values()) {
-			if (s != Squadre.Inter) {
-				nomesquadra.add(new JLabel("" + s));
-				imgStemma.add(new ImageIcon((new ImageIcon(getClass().getResource("/Stemma_" + s + ".png"))).getImage()
-						.getScaledInstance(dim.width / 15, dim.height / 12, java.awt.Image.SCALE_SMOOTH)));
+		top.add(titolo,BorderLayout.CENTER);
+		top.add(home,BorderLayout.EAST);
+		
+		for(Squadre s: Squadre.values()) {
+			if(s!=Squadre.Inter) {
+				nomesquadra.add(new JLabel(""+s));
+				imgStemma.add(new ImageIcon((new ImageIcon(getClass().getResource("/Stemma_"+s+".png"))).getImage().getScaledInstance(dim.width/15, dim.height/12, java.awt.Image.SCALE_SMOOTH)));
 			}
 		}
-
-		JPanel selezionesquadra = new JPanel();
-
-		selezionesquadra.setPreferredSize(new Dimension(dim.width, dim.height / 3));
-		selezionesquadra.setLayout(new GridLayout(2, 10));
-
-		for (int i = 0; i < 19; i++) {
+		
+		JPanel selezionesquadra=new JPanel();
+		
+		selezionesquadra.setPreferredSize(new Dimension(dim.width,dim.height/3));
+		selezionesquadra.setLayout(new GridLayout(2,10));
+		
+		for(int i=0;i<19;i++) {
 			nomesquadra.get(i).setHorizontalAlignment((int) CENTER_ALIGNMENT);
-			sceltasquadra.add(new SquadraButton(imgStemma.get(i), false));
+			sceltasquadra.add(new SquadraButton(imgStemma.get(i),false));
 			squadra.add(new JPanel());
 			squadra.get(i).setLayout(new BorderLayout());
-			squadra.get(i).add(sceltasquadra.get(i), BorderLayout.CENTER);
-			squadra.get(i).add(nomesquadra.get(i), BorderLayout.SOUTH);
-			squadra.get(i).setBorder(BorderFactory.createLineBorder(Color.black, 5));
+			squadra.get(i).add(sceltasquadra.get(i),BorderLayout.CENTER);
+			squadra.get(i).add(nomesquadra.get(i),BorderLayout.SOUTH);
+			squadra.get(i).setBorder(BorderFactory.createLineBorder(Color.black,5));
 			selezionesquadra.add(squadra.get(i));
 		}
-
-		selezionesquadra.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
+		
+		selezionesquadra.setBorder(BorderFactory.createEmptyBorder(20,0,20,0));
 		selezionesquadra.setBackground(Color.black);
-
-		JPanel info = new JPanel();
-
-		JLabel d = new JLabel("Data:");
+		
+		JPanel info=new JPanel();
+		
+		JLabel d=new JLabel("Data:");
 		d.setFont(medium);
-
-		JLabel h = new JLabel("Ora:");
+		
+		JLabel h=new JLabel("Ora:");
 		h.setFont(medium);
-		errorLabel = new JLabel(" ");
+		errorLabel=new JLabel(" ");
 		errorLabel.setForeground(Color.RED);
-
-		aggiungi = new JButton("AGGIUNGI");
+		
+		
+		aggiungi=new JButton("AGGIUNGI");
 		aggiungi.setFont(medium);
-
-		info.setLayout(new GridBagLayout());
+		
+		info.setLayout(new GridBagLayout());		
 		GridBagConstraints infoConstraints = new GridBagConstraints();
-		infoConstraints.insets = new Insets(3, 3, 20, 15);
+		infoConstraints.insets = new Insets(3,3,20,15);
 
 		infoConstraints.gridwidth = 1;
 		infoConstraints.gridx = 0;
@@ -142,82 +138,93 @@ public class AdminAddPartiteView extends AView {
 		infoConstraints.gridx = 0;
 		infoConstraints.gridy = 3;
 		info.add(aggiungi, infoConstraints);
+		
+		JPanel middle=new JPanel();
 
-		JPanel middle = new JPanel();
-
-		middle.setPreferredSize(new Dimension(dim.width, dim.height / 2));
-		middle.setLayout(new GridLayout(2, 1));
+		middle.setPreferredSize(new Dimension(dim.width,dim.height/2));
+		middle.setLayout(new GridLayout(2,1));
 		middle.add(selezionesquadra);
 		middle.add(info);
-
+		
+		
 		this.setLayout(new BorderLayout());
-		this.add(top, BorderLayout.NORTH);
-		this.add(middle, BorderLayout.CENTER);
+		this.add(top,BorderLayout.NORTH);
+		this.add(middle,BorderLayout.CENTER);
 	}
-
-	/**
-	 *
-	 * @return Bottone per aggiungere una partita.
-	 */
-
-	public JButton getAggiungiButton() {
-		return aggiungi;
-	}
-
-	/**
-	 *
-	 * @return Bottoni di scelta squadra.
-	 */
+	
+	
 	public Collection<SquadraButton> getButtons() {
 		return sceltasquadra;
 	}
-
-	public LocalDate getData() {
-		return data.getDatePicker().getDate();
+	
+	public JButton getAggiungiButton() {
+		return aggiungi;
 	}
-
-	public JButton getHome() {
-		return home;
+	
+	public void setUso() {
+		for(int i=0;i<19;i++) {
+			sceltasquadra.get(i).setUso(false);
+		}
 	}
-
-	public String getOra() {
-		return (String) orario.getSelectedItem();
+	public JButton getAggiungi() {
+		return aggiungi;
 	}
-
-	/**
-	 * Metodo utilizzato per capire con quale squadra ly
-	 *
-	 * @return Stringa della squadra selezionata.
-	 */
+	
 	public String getSquadraScelta() {
-		for (int i = 0; i < 19; i++) {
-			if (sceltasquadra.get(i).getUso()) {
-
+		for(int i=0;i<19;i++) {
+			if(sceltasquadra.get(i).getUso()==true) {
+				
 				return nomesquadra.get(i).getText();
 			}
 		}
 		return null;
 	}
-
-	public void onAddError(String errore) {
-		errorLabel.setText(errore);
+	
+	public LocalDate getData() {
+		return data.getDate();
 	}
-
+	
+	public String getOra() {
+		return (String) orario.getSelectedItem();
+	}
+	
+	public JButton getHome() {
+		return home;
+	}
+	
+	public void setSquadra() {
+		for(int i=0;i<19;i++) {
+			if(sceltasquadra.get(i).getUso()==true) {
+				sceltasquadra.get(i).setBackground(Color.GREEN);
+			}else {
+				sceltasquadra.get(i).setBackground(Color.WHITE);
+			}
+		}
+		
+	}
+	
 	public void onSquadError(String errore) {
 		errorLabel.setText(errore);
 	}
-
-	@Override
+	
+	public void onAddError(String errore) {
+		errorLabel.setText(errore);
+	}
+	
+	public void success(String string) {
+		errorLabel.setForeground(Color.GREEN);
+		errorLabel.setText(string);
+	}
+	
 	public void onWindowResized(Dimension dim) {
-
+		
 		imgStemma.clear();
-		for (Squadre s : Squadre.values()) {
-			if (s != Squadre.Inter) {
-				imgStemma.add(new ImageIcon((new ImageIcon(getClass().getResource("/Stemma_" + s + ".png"))).getImage()
-						.getScaledInstance(dim.width / 15, dim.height / 12, java.awt.Image.SCALE_SMOOTH)));
+		for(Squadre s: Squadre.values()) {
+			if(s!=Squadre.Inter) {
+				imgStemma.add(new ImageIcon((new ImageIcon(getClass().getResource("/Stemma_"+s+".png"))).getImage().getScaledInstance(dim.width/15, dim.height/12, java.awt.Image.SCALE_SMOOTH)));
 			}
 		}
-		for (int i = 0; i < 19; i++) {
+		for(int i=0;i<19;i++) {
 			sceltasquadra.get(i).modificaImg(imgStemma.get(i));
 			squadra.get(i).revalidate();
 			squadra.get(i).repaint();
@@ -225,33 +232,12 @@ public class AdminAddPartiteView extends AView {
 
 		this.revalidate();
 		this.repaint();
-
+		
 	}
 
-	public void setSquadra() {
-		for (int i = 0; i < 19; i++) {
-			if (sceltasquadra.get(i).getUso()) {
-				sceltasquadra.get(i).setBackground(Color.GREEN);
-			} else {
-				sceltasquadra.get(i).setBackground(Color.WHITE);
-			}
-		}
 
-	}
-
-	/**
-	 * metodo chiamato quando voglio rimettere a false lo stato di utilizzo di una
-	 * squadra.
-	 */
-	public void setUso() {
-		for (int i = 0; i < 19; i++) {
-			sceltasquadra.get(i).setUso(false);
-		}
-	}
-
-	public void success(String string) {
-		errorLabel.setForeground(Color.GREEN);
-		errorLabel.setText(string);
-	}
+	
+	
+	
 
 }
